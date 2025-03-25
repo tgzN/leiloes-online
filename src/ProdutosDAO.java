cd1c1077d9e7d2f9f35d9cf4df95471ea38cf159
Tiago Cordeiro <109906809+tgzN@users.noreply.github.com>
2025-03-24 22:54:54 -0300

    Commit #1: adicionado o botão Vender.

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -55,10 +55,38 @@ public class ProdutosDAO {
         return listagem;
     }
     
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+        ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = conectaDAO.connectDB();
+            String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(rs.getInt("id"));
+                produto.setNome(rs.getString("nome"));
+                produto.setValor(rs.getInt("valor"));
+                produto.setStatus(rs.getString("status"));
+                listagem.add(produto);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + e.getMessage());
+        } finally {
+            conectaDAO.closeConnection(conn, pstmt, rs); // Feche a conexão
+        }
+        return listagem;
+    }
+    
     public void venderProduto(int id) {
         Connection conn = null;
         PreparedStatement pstmt = null;
-
+        // para o commit :flushed:
         try {
             conn = conectaDAO.connectDB();
             String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
